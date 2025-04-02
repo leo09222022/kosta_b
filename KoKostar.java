81a148ca895466b1c07609df334714e9391b0500
Sana2d2v <sana2d2v@gmail.com>
2025-04-02 10:03:43 +0900

    feat: 꼬르르륵….

--- KoKostar.java
+++ KoKostar.java
@@ -2,6 +2,8 @@ class KoKostar {
 
 	public static void main(String[] args) {
 		System.out.println("점심 돈까스!!! 김볶밥~");
+		MemberDAO dao = new MemberDAO();
+				dao.insert();
 	}
 
 }
---
+++ MemberDAO.java
@@ -0,0 +1,5 @@
+class MemberDAO{
+public void insert(){
+System.out.println("맛도리~웅냠냠");
+}
+}
